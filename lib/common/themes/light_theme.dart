04d0de9abf9b7c3a89784b12295ac4421f7dc9bf
Ghaith <ghayth.1900@gmail.com>
2025-10-03 18:48:59 +0200

    setup light mode

--- lib/common/themes/light_theme.dart
+++ lib/common/themes/light_theme.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
-  colorScheme: ColorScheme.light(),
+  colorScheme: ColorScheme.light(
+      background: Colors.grey.shade300,
+      secondary: Colors.grey.shade100,
+      primary: Colors.grey.shade50,
+      tertiary: Colors.white,
+      inversePrimary: Colors.grey.shade700),
 );
