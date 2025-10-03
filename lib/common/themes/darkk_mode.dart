d9b822c3ae11aa0f7e80481a50955336a4f9eff8
Ghaith <ghayth.1900@gmail.com>
2025-10-03 18:54:43 +0200

    setup darrk mode

--- lib/common/themes/darkk_mode.dart
+++ lib/common/themes/darkk_mode.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    secondary: Colors.grey.shade100,
+    primary: Colors.grey.shade50,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
