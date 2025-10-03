c2bbd0f59820a08412e55c8ec8ffe4395c1fd00e
Ghaith <ghayth.1900@gmail.com>
2025-10-03 20:43:01 +0200

    setup darrk mode

---
+++ lib/common/themes/theme_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/common/themes/darkk_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkmode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void ToggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
