6ba15ddd0fa17c422003a96e5c5ca9015c19a811
Ghaith <ghayth.1900@gmail.com>
2025-10-03 17:50:44 +0200

    working  on the  splash  sceen

---
+++ lib/screens/spalsh_scrreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpalshScrreen extends StatefulWidget {
+  const SpalshScrreen({super.key});
+
+  @override
+  State<SpalshScrreen> createState() => _SpalshScrreenState();
+}
+
+class _SpalshScrreenState extends State<SpalshScrreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
