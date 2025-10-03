3cf6fb885f9ea6ddb1f69a0f1325ef1d7a14d790
Ghaith <ghayth.1900@gmail.com>
2025-10-03 17:56:43 +0200

    working  on the  splash  sceen

---
+++ lib/common/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0XffFC6011);
+  static const Color primarytext = Color(0Xff4A4B4D);
+  static const Color textfield = Color(0XffF2F2F2);
+  static const Color secondarytext = Color(0Xff7C7D7E);
+  static const Color placeholder = Color(0XffB6B7B7);
+}
