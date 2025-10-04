4d006fbb668c9fd4ccb9f0b238796ac3e804e783
Ghaith <ghayth.1900@gmail.com>
2025-10-04 04:00:49 +0200

    working on componants

---
+++ lib/widgets/m_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyButtonWidget extends StatelessWidget {
+  const MyButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/widgets/text_form_fieldwidgert.dart
+++ lib/widgets/text_form_fieldwidgert.dart
@@ -1,24 +1,53 @@
 import 'package:flutter/material.dart';
 
-class TextFormFieldwidget extends StatelessWidget {
+class TextFormFieldwidget extends StatefulWidget {
   const TextFormFieldwidget(
       {super.key,
-      required this.isobscuretext,
+      required this.ispassword,
       this.suffixicon,
       required this.label,
       this.validator,
       required this.controller});
-  final bool isobscuretext;
+  final bool ispassword;
   final IconData? suffixicon;
   final String label;
   final VoidCallback? validator;
   final TextEditingController controller;
+
+  @override
+  State<TextFormFieldwidget> createState() => _TextFormFieldwidgetState();
+}
+
+class _TextFormFieldwidgetState extends State<TextFormFieldwidget> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: label,
+    bool isobscureText = true;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22),
+      child: TextFormField(
+        controller: widget.controller,
+        obscureText: widget.ispassword ? isobscureText : false,
+        decoration: InputDecoration(
+          suffixIcon: widget.ispassword
+              ? IconButton(
+                  onPressed: () => setState(() {
+                        isobscureText = !isobscureText;
+                      }),
+                  icon: Icon(widget.ispassword
+                      ? Icons.visibility_off
+                      : Icons.visibility))
+              : null,
+          hintText: widget.label,
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide:
+                  BorderSide(color: Theme.of(context).colorScheme.tertiary)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide:
+                  BorderSide(color: Theme.of(context).colorScheme.primary)),
+        ),
       ),
     );
   }
