4c2991982edff14d618448f4355f7c823219bdf7
Ghaith <ghayth.1900@gmail.com>
2025-10-04 03:13:28 +0200

    working on componants

--- lib/widgets/text_form_fieldwidgert.dart
+++ lib/widgets/text_form_fieldwidgert.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class TextFormFieldwidget extends StatelessWidget {
-  const TextFormFieldwidget({super.key});
+  const TextFormFieldwidget(
+      {super.key,
+      required this.isobscuretext,
+      this.suffixicon,
+      required this.label,
+      this.validator,
+      required this.controller});
   final bool isobscuretext;
   final IconData? suffixicon;
   final String label;
   final VoidCallback? validator;
+  final TextEditingController controller;
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: label,
+      ),
+    );
   }
 }
