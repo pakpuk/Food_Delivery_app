8f234d8dba5aac9c91e0a879fec0b060bfc20f0d
Ghaith <ghayth.1900@gmail.com>
2025-10-05 14:29:52 +0200

    working on home page

--- lib/common/text_manager.dart
+++ lib/common/text_manager.dart
@@ -1,4 +1,8 @@
 class TextManager {
   static const String welcometxt = 'Welcome To our Food  app';
   static const String logintxt = ' Login';
+  static const String signuptxt = ' Signup';
+  static const String udonthaveaccounttxt = ' You dont have an account ?';
+  static const String uhaveaccounttxt = ' You already have an account ?';
+  static const String registre = 'Register Here';
 }
