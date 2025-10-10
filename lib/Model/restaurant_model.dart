14c89f26901a0fedf9dd34cb60598393e9eb1f2b
Ghaith <ghayth.1900@gmail.com>
2025-10-10 23:52:43 +0200

    working  on  the  main  logic of  the  app

--- lib/Model/restaurant_model.dart
+++ lib/Model/restaurant_model.dart
@@ -1,1 +1,50 @@
-class RestaurantModel {}
+import 'package:food_delivery_app/Model/food_model.dart';
+
+class RestaurantModel {
+  final List<FoodModel> menu = [
+    FoodModel(
+        name: 'burger',
+        imagepath: "imagepath",
+        price: 4.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'Double Meal Burger',
+        imagepath: "imagepath",
+        price: 8.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: ' Blue moon burger',
+        imagepath: "imagepath",
+        price: 4.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'burger',
+        imagepath: "imagepath",
+        price: 4.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+  ];
+}
