67a1add9c290df8a03b8dd98640f67523e94b44b
Ghaith <ghayth.1900@gmail.com>
2025-10-11 00:07:21 +0200

    working  on  the  main  logic of  the  app

--- lib/Model/restaurant_model.dart
+++ lib/Model/restaurant_model.dart
@@ -6,7 +6,7 @@ class RestaurantModel {
         name: 'burger',
         imagepath: "imagepath",
         price: 4.99,
-        discription: "A burger with viandachee",
+        discription: "A  delicious burger with meal ",
         availabeAddones: [
           Addon(name: 'Extra cheese', price: 0.99),
           Addon(name: 'Extra met', price: 1.99),
@@ -17,7 +17,7 @@ class RestaurantModel {
         name: 'Double Meal Burger',
         imagepath: "imagepath",
         price: 8.99,
-        discription: "A burger with viandachee",
+        discription: "A burger with  double  viandachee",
         availabeAddones: [
           Addon(name: 'Extra cheese', price: 0.99),
           Addon(name: 'Extra met', price: 1.99),
@@ -26,6 +26,28 @@ class RestaurantModel {
         category: FoodCategories.burgers),
     FoodModel(
         name: ' Blue moon burger',
+        imagepath: "imagepath",
+        price: 9.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: ' Cheees burger',
+        imagepath: "imagepath",
+        price: 4.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'Salade',
         imagepath: "imagepath",
         price: 4.99,
         discription: "A burger with viandachee",
@@ -39,6 +61,50 @@ class RestaurantModel {
         name: 'burger',
         imagepath: "imagepath",
         price: 4.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'burger',
+        imagepath: "imagepath",
+        price: 4.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'drink',
+        imagepath: "imagepath",
+        price: 1.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'Donuts',
+        imagepath: "imagepath",
+        price: 1.99,
+        discription: "A burger with viandachee",
+        availabeAddones: [
+          Addon(name: 'Extra cheese', price: 0.99),
+          Addon(name: 'Extra met', price: 1.99),
+          Addon(name: 'Extra Sauce', price: 0.99),
+        ],
+        category: FoodCategories.burgers),
+    FoodModel(
+        name: 'Macarron',
+        imagepath: "imagepath",
+        price: 1.22,
         discription: "A burger with viandachee",
         availabeAddones: [
           Addon(name: 'Extra cheese', price: 0.99),
