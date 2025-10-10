0a5d096d1503354e166b24e994c18cdd865bad49
Ghaith <ghayth.1900@gmail.com>
2025-10-10 23:19:21 +0200

    finish food  model

--- lib/Model/food_model.dart
+++ lib/Model/food_model.dart
@@ -1,1 +1,30 @@
-class FoodModel {}
+class FoodModel {
+  final String name;
+  final String imagepath;
+  final double price;
+  final String discription;
+  final FoodCategories category;
+  final List<Addon> availabeAddones;
+
+  FoodModel(
+      {required this.name,
+      required this.imagepath,
+      required this.price,
+      required this.discription,
+      required this.availabeAddones,
+      required this.category});
+}
+
+enum FoodCategories {
+  burgers,
+  salads,
+  sides,
+  drinks,
+  desserts,
+}
+
+class Addon {
+  String name;
+  double price;
+  Addon({required this.name, required this.price});
+}
