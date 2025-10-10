35f89b3844cf637bc9e674277891ff8cf2304a9a
Ghaith <ghayth.1900@gmail.com>
2025-10-10 22:05:29 +0200

    working on the  main design

---
+++ lib/widgets/my_tab_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyTabBar extends StatelessWidget {
+  final TabController tabBarController;
+  const MyTabBar({super.key, required this.tabBarController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
