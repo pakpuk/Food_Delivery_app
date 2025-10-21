a5bb4bbbd22791a6459eecc5e87179b21e99d4fc
Ghaith <ghayth.1900@gmail.com>
2025-10-21 22:56:56 +0200

    start working  on  fooddeatails page

---
+++ lib/screens/food_page_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodPageDetails extends StatefulWidget {
+  const FoodPageDetails({super.key});
+
+  @override
+  State<FoodPageDetails> createState() => _FoodPageDetailsState();
+}
+
+class _FoodPageDetailsState extends State<FoodPageDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
