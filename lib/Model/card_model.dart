235943e824ec6ebda9bf4bd63c96e9d6cff59391
Ghaith <ghayth.1900@gmail.com>
2025-10-23 23:31:55 +0200

    finish  food details  page

--- lib/Model/card_model.dart
+++ lib/Model/card_model.dart
@@ -1,1 +1,13 @@
-class CardModel {}
+import 'package:food_delivery_app/Model/food_model.dart';
+
+class CardModel {
+  final FoodModel food;
+  final List<Addon> selectedAddons;
+  final int quantiy;
+
+  CardModel({
+    required this.food,
+    required this.selectedAddons,
+    this.quantiy = 1,
+  });
+}
