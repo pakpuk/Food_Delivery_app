0ced30665588e6cb3eeca53b809b968d7fa9b9ab
Ghaith <ghayth.1900@gmail.com>
2025-10-23 19:40:13 +0200

    almost finish  food details  page

--- lib/screens/food_page_details.dart
+++ lib/screens/food_page_details.dart
@@ -19,6 +19,24 @@ class _FoodPageDetailsState extends State<FoodPageDetails> {
       body: SafeArea(
         child: Column(
           children: [
+            Image.asset(widget.food.imagepath),
+            Text(
+              widget.food.name,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            Text(
+              widget.food.price.toString(),
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                  color: Theme.of(context).colorScheme.primary),
+            ),
+            Text(
+              widget.food.discription,
+            ),
             Padding(
               padding: const EdgeInsets.all(22.0),
               child: Column(
