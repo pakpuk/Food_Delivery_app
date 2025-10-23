3221cc44f6596f40601084bdcae620e30f337c4c
Ghaith <ghayth.1900@gmail.com>
2025-10-23 19:24:15 +0200

    start working  on  fooddeatails page

--- lib/screens/food_page_details.dart
+++ lib/screens/food_page_details.dart
@@ -15,6 +15,10 @@ class FoodPageDetails extends StatefulWidget {
 class _FoodPageDetailsState extends State<FoodPageDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
