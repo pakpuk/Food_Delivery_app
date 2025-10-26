951337ea532e08e764cae6c7092ab1b48b4af579
Ghaith <ghayth.1900@gmail.com>
2025-10-26 17:02:38 +0100

    keep working

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/Model/card_model.dart';
+import 'package:food_delivery_app/Model/food_model.dart';
 
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
-
+  const CartPage({super.key, required this.food, required this.cardItem});
+  final FoodModel food;
+  final CardModel cardItem;
   @override
   State<CartPage> createState() => _CartPageState();
 }
