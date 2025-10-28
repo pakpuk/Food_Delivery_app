6221c067b10e9d8a457f02107cdc4076ce505ab2
Ghaith <ghayth.1900@gmail.com>
2025-10-28 20:52:21 +0100

    almost ifnish  the  app

--- lib/widgets/my_recipt.dart
+++ lib/widgets/my_recipt.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/Model/restaurant_model.dart';
+import 'package:provider/provider.dart';
 
 class MyRecipt extends StatelessWidget {
   const MyRecipt({super.key});
@@ -19,14 +20,24 @@ class MyRecipt extends StatelessWidget {
                 fontSize: 22,
               ),
             ),
+            const SizedBox(
+              height: 25,
+            ),
             Container(
               padding: const EdgeInsets.all(25),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(12),
                   border: Border.all(
                       color: Theme.of(context).colorScheme.secondary)),
-              child: Consumer<RestaurantModel>(builder:(context,restaurant,child)=>return Text(restaurant.di)){},
-            )
+              child: Consumer<RestaurantModel>(
+                builder: (context, restaurant, child) =>
+                    Text(restaurant.displayCartRecipt()),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+          const   Text('Esitamted Delivery time is : 4:10 PM');
           ],
         ),
       ),
